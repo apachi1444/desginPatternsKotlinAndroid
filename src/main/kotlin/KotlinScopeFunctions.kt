806410eba27ce1abea0d9938fffec5a5a6f824e5
Apachi144 <yessinejawa@gmail.com>
2022-09-26 21:54:00 +0100

    adding scope functions keywords

---
+++ src/main/kotlin/KotlinScopeFunctions.kt
@@ -0,0 +1,7 @@
+var letKeyword : String = "let us to make instructions on variables and checks if their not null"
+
+var letExample : String = "val x = number?.let{it = it+1} it returns the last line ( in this case it will be int )"
+
+var applyKeyword : String = "take an object and make some changes on it and returns a new object"
+
+var runKeyword : String = "take an object do some modifications and return nothing"
